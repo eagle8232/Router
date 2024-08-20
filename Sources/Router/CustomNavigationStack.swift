fe514a7336409ff47ae5d1e4472f15fd94e95d24
Vusal <zevs.1418@gmail.com>
2024-08-21 01:10:00 +0400

    Title Parameter Update - 3

--- Sources/Router/CustomNavigationStack.swift
+++ Sources/Router/CustomNavigationStack.swift
@@ -13,14 +13,22 @@ public struct CustomNavigationStack<C: View>: UIViewControllerRepresentable {
     @ObservedObject var router: Router = Router()
     @ViewBuilder var rootView: () -> C
     var title: String?
+    var largeTitleDisplayMode: UINavigationItem.LargeTitleDisplayMode
     
-    public init(title: String? = nil, @ViewBuilder root: @escaping () -> C) {
+    public init(title: String? = nil,
+                largeTitleDisplayMode: UINavigationItem.LargeTitleDisplayMode = .always,
+                @ViewBuilder root: @escaping () -> C) {
+        
         self.title = title
+        self.largeTitleDisplayMode = largeTitleDisplayMode
         self.rootView = root
     }
     
     public func makeUIViewController(context: Context) -> UINavigationController {
-        return self.router.setupNavigationController(with: rootView(), title: title)
+        return self.router.setupNavigationController(
+            with: rootView(),
+            title: title, largeTitleDisplayMode:
+                largeTitleDisplayMode)
     }
     
     public func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
