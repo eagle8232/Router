c23d596a986011620d3d9daa4c6b1a685120090d
Vusal <zevs.1418@gmail.com>
2024-08-25 06:31:14 +0400

    Title Bug Fix Update

---
+++ Sources/Router/Extensions/Notification.Name + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Vusal Nuriyev 2 on 25.08.24.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let languageChanged = Notification.Name("languageChanged")
+}
